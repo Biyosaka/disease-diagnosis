2525d19e99aef3f3b418d51c29b74b55d2de2b68
Biyosaka <91135399+Biyosaka@users.noreply.github.com>
2024-08-05 11:19:03 +0700

    Update app.py

--- steamlit_web/app.py
+++ steamlit_web/app.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder, MultiLabelBinar
 with open('steamlit_web/Logistic_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-with open('ssteamlit_web/scaler3 (1).pkl', 'rb') as f:
+with open('ssteamlit_web/scaler.pkl', 'rb') as f:
     scaler = pickle.load(f)
 
 with open('steamlit_web/symptom4.pkl', 'rb') as f:
